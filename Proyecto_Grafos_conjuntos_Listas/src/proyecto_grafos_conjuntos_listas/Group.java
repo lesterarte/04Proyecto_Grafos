d5cd307d7f2a24c1749e5b958bd312c3944148c6
Oscar David Díaz Hernández <oscar.dh@unitec.edu>
2016-09-14 13:41:29 -0600

    Modicacion clase Group.java

--- Proyecto_Grafos_conjuntos_Listas/src/proyecto_grafos_conjuntos_listas/Group.java
+++ Proyecto_Grafos_conjuntos_Listas/src/proyecto_grafos_conjuntos_listas/Group.java
@@ -4,17 +4,26 @@ package proyecto_grafos_conjuntos_listas;
 
 public class Group {
 
-    private int cantCouples;
-    private TDA_Set Member; 
+    private int cantCouples = 0;
+    private TDA_Set Members;
+    private Member Leader;
     
-    public Group(int cantCouples, TDA_Set Member) {
+    public Group(int cantCouples, TDA_Set Members) {
         this.cantCouples = cantCouples;
-        this.Member = Member;
+        this.Members = Members;
     }
 
     public Group() {
     }
 
+    public Member getLeader() {
+        return Leader;
+    }
+
+    public void setLeader(Member Leader) {
+        this.Leader = Leader;
+    }
+    
     public int getCantCouples() {
         return cantCouples;
     }
@@ -23,25 +32,27 @@ public class Group {
         this.cantCouples = cantCouples;
     }
 
-    public TDA_Set getMember() {
-        return Member;
+    public TDA_Set getMembers() {
+        return Members;
     }
 
-    public void setMember(TDA_Set Member) {
-        this.Member = Member;
+    public void setMembers(TDA_Set Members) {
+        this.Members = Members;
     }
     
     public boolean hasCouple(){
-        
-        return true;    
+        return cantCouples > 0;    
     }  
     
-    public int cantidadMiembros(){
-        return 0;
+    public int cantMembers(){
+        return this.Members.getCountKies();
     }
     
     public void addMember(Member newMember){
-        
+        this.Members.put(newMember.getName(), newMember);
+        if (newMember.isCouple()) {
+            this.cantCouples++;
+        }
     }  
     
 }
